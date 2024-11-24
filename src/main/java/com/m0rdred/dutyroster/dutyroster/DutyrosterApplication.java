2cce4f1636c59cc0570260596c359ffd42e222ba
Daniel Szabo <daniel.szabo@lhsystems.com>
2024-11-24 23:12:46 +0100

    springboot project init

---
+++ src/main/java/com/m0rdred/dutyroster/dutyroster/DutyrosterApplication.java
@@ -0,0 +1,13 @@
+package com.m0rdred.dutyroster.dutyroster;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DutyrosterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DutyrosterApplication.class, args);
+	}
+
+}
