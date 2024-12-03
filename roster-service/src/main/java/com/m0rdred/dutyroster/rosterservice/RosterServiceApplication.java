3ffd64043806490299d8ade188f98dbb1ba9ad8c
Daniel Szabo <daniel.szabo@lhsystems.com>
2024-12-03 21:54:06 +0100

    springboot project init

---
+++ roster-service/src/main/java/com/m0rdred/dutyroster/rosterservice/RosterServiceApplication.java
@@ -0,0 +1,13 @@
+package com.m0rdred.dutyroster.rosterservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RosterServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RosterServiceApplication.class, args);
+	}
+
+}
---
+++ roster-service/src/test/java/com/m0rdred/dutyroster/rosterservice/RosterServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.m0rdred.dutyroster.rosterservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RosterServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
